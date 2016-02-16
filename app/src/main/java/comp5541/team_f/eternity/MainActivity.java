9a07685866c8d4d7f5c47b62ee84405fb5720b99
Dick Smith <git@dicksmith.ca>
2016-02-15 20:27:35 -0500

    Refactoring of Digits and Operators; Clear/CE

--- app/src/main/java/comp5541/team_f/eternity/MainActivity.java
+++ app/src/main/java/comp5541/team_f/eternity/MainActivity.java
@@ -2,6 +2,7 @@ package comp5541.team_f.eternity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -30,11 +31,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        previous = new StringBuilder("0");
         current = new StringBuilder();
-        previous = new StringBuilder();
         tvPrevious = (TextView) findViewById(R.id.tvPrevious);
+        tvPrevious.setMovementMethod(new ScrollingMovementMethod());
         tvCurrent = (TextView) findViewById(R.id.tvCurrent);
+//        tvCurrent.setMovementMethod(new ScrollingMovementMethod());
         tvCurrent.setText("0");
+        parenthesesDepth = 0;
 
         btn0 = (Button) findViewById(R.id.btn0);
         btn1 = (Button) findViewById(R.id.btn1);
@@ -68,92 +72,120 @@ public class MainActivity extends AppCompatActivity {
         btn0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                current.append("0");
-                tvCurrent.setText(current);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*[\\+\\-\\*/%\\(]$)", current.toString())) {
+                    current.append("0");
+                    tvCurrent.setText(current);
+                }
             }
         });
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                current.append("1");
-                tvCurrent.setText(current);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*[\\+\\-\\*/%\\(]$)", current.toString())) {
+                    current.append("1");
+                    tvCurrent.setText(current);
+                }
             }
         });
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                current.append("2");
-                tvCurrent.setText(current);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*[\\+\\-\\*/%\\(]$)", current.toString())) {
+                    current.append("2");
+                    tvCurrent.setText(current);
+                }
             }
         });
 
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                current.append("3");
-                tvCurrent.setText(current);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*[\\+\\-\\*/%\\(]$)", current.toString())) {
+                    current.append("3");
+                    tvCurrent.setText(current);
+                }
             }
         });
 
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                current.append("4");
-                tvCurrent.setText(current);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*[\\+\\-\\*/%\\(]$)", current.toString())) {
+                    current.append("4");
+                    tvCurrent.setText(current);
+                }
             }
         });
 
         btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View v) {
-                current.append("5");
-                tvCurrent.setText(current);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*[\\+\\-\\*/%\\(]$)", current.toString())) {
+                    current.append("5");
+                    tvCurrent.setText(current);
+                }
             }
         });
 
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                current.append("6");
-                tvCurrent.setText(current);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*[\\+\\-\\*/%\\(]$)", current.toString())) {
+                    current.append("6");
+                    tvCurrent.setText(current);
+                }
             }
         });
 
         btn7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                current.append("7");
-                tvCurrent.setText(current);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*[\\+\\-\\*/%\\(]$)", current.toString())) {
+                    current.append("7");
+                    tvCurrent.setText(current);
+                }
             }
         });
 
         btn8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                current.append("8");
-                tvCurrent.setText(current);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*[\\+\\-\\*/%\\(]$)", current.toString())) {
+                    current.append("8");
+                    tvCurrent.setText(current);
+                }
             }
         });
 
         btn9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                current.append("9");
-                tvCurrent.setText(current);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*[\\+\\-\\*/%\\(]$)", current.toString())) {
+                    current.append("9");
+                    tvCurrent.setText(current);
+                }
             }
         });
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (current.length() != 0 && Pattern.matches("\\D|[^\\(]", ((Character) current.charAt(current.length() - 1)).toString()))
-                    return;
-                if (Pattern.matches("\\.", ((Character) current.charAt(current.length() - 1)).toString()))
-                    current.append("0");
-                else current.append("+");
-                tvCurrent.setText(current);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*\\)$)", current.toString())) {
+                    current.append("+");
+                    tvCurrent.setText(current);
+                }
+            }
+        });
+
+        btnCA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                previous = new StringBuilder("0");
+                current = new StringBuilder();
+                tvPrevious.setText("");
+                tvCurrent.setText("0");
+                parenthesesDepth = 0;
             }
         });
 
@@ -162,64 +194,96 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 current = new StringBuilder();
                 tvCurrent.setText("0");
+                parenthesesDepth = 0;
             }
         });
 
-        btnCA.setOnClickListener(new View.OnClickListener() {
+        btnDiv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                current = new StringBuilder();
-                tvCurrent.setText("0");
-                previous = new StringBuilder();
-                tvPrevious.setText("");
-            }
-        });
-
-        btnBS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (current.length() < 1) tvCurrent.setText("0");
-                current.deleteCharAt(current.length() - 1);
-                tvCurrent.setText(current);
-            }
-        });
-
-        btnDot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                current.append(".");
-                tvCurrent.setText(current);
-            }
-        });
-
-        btnNeg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (current.charAt(0) == ' ' && current.charAt(1) == '-') current.delete(0, 1);
-                else current.insert(0, " -");
-                tvCurrent.setText(current);
-            }
-        });
-
-        btnEqu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (current.length() == 0) return;
-                if (Pattern.matches("\\d|\\s", ((Character) current.charAt(0)).toString())) {
-                    previous.delete(0, previous.length());
-                    previous.append(current);
-                    tvPrevious.setText(previous);
-                } else {
-                    if (previous.length() == 0) previous.append("0");
-                    previous.append(current);
-                    tvPrevious.setText(previous);
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*\\)$)", current.toString())) {
+                    current.append("/");
+                    tvCurrent.setText(current);
                 }
-                if (Pattern.matches("\\.", ((Character) current.charAt(current.length() - 1)).toString()))
-                    current.append("0");
-                Expression e = new ExpressionBuilder(previous.toString()).build();
-                current = new StringBuilder(((Double) e.evaluate()).toString());
-                tvCurrent.setText(current);
             }
         });
+
+        btnMod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*\\)$)", current.toString())) {
+                    current.append("%");
+                    tvCurrent.setText(current);
+                }
+            }
+        });
+
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*\\)$)", current.toString())) {
+                    current.append("*");
+                    tvCurrent.setText(current);
+                }
+            }
+        });
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Pattern.matches("(^$)|(.*\\d$)|(.*\\)$)", current.toString())) {
+                    current.append("-");
+                    tvCurrent.setText(current);
+                }
+            }
+        });
+
+
+//        btnBS.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (current.length() < 1) tvCurrent.setText("0");
+//                current.deleteCharAt(current.length() - 1);
+//                tvCurrent.setText(current);
+//            }
+//        });
+
+//        btnDot.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                current.append(".");
+//                tvCurrent.setText(current);
+//            }
+//        });
+
+//        btnNeg.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (current.charAt(0) == ' ' && current.charAt(1) == '-') current.delete(0, 1);
+//                else current.insert(0, " -");
+//                tvCurrent.setText(current);
+//            }
+//        });
+
+//        btnEqu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (current.length() == 0) return;
+//                if (Pattern.matches("\\d|\\s", ((Character) current.charAt(0)).toString())) {
+//                    previous.delete(0, previous.length());
+//                    previous.append(current);
+//                    tvPrevious.setText(previous);
+//                } else {
+//                    if (previous.length() == 0) previous.append("0");
+//                    previous.append(current);
+//                    tvPrevious.setText(previous);
+//                }
+//                if (Pattern.matches("\\.", ((Character) current.charAt(current.length() - 1)).toString()))
+//                    current.append("0");
+//                Expression e = new ExpressionBuilder(previous.toString()).build();
+//                current = new StringBuilder(((Double) e.evaluate()).toString());
+//                tvCurrent.setText(current);
+//            }
+//        });
     }
 }
