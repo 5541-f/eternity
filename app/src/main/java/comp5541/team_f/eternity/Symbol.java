0702376fd3a221165edf10f00325f8a9cc098c10
Dick Smith <git@dicksmith.ca>
2016-02-20 15:16:33 -0500

    Minor visual change.

--- app/src/main/java/comp5541/team_f/eternity/Symbol.java
+++ app/src/main/java/comp5541/team_f/eternity/Symbol.java
@@ -2,12 +2,12 @@ package comp5541.team_f.eternity;
 
 enum Symbol {
 
-    SINE             ("∿",  "<tt><small>sin</small></tt>(",         "sin("),//rename later
-    EXPONENT_NATURAL ("ℯ",  "<tt><small><i>ℯ</i>ˣ</small></tt>(",   "exp("),//rename later
-    SQUARE_ROOT      ("√",  "<tt>√</tt>(",                         "sqrt("), //rename later
-    EXPONENT_10      ("⑽",  "<tt><small>10ˣ</small></tt>(",        "10^("),//rename later
-    LOGARITHM_10     ("㏒",  "<tt><small>log₁₀</small></tt>(",       "log10("), //rename later
-    MODULUS          ("%",  "<tt><small>mod</small></tt>",          "%"),
+    SINE             ("∿",  " <tt><small>sin</small></tt>(",         "sin("),//rename later
+    EXPONENT_NATURAL ("ℯ",  " <tt><small><i>ℯ</i>ˣ</small></tt>(",   "exp("),//rename later
+    SQUARE_ROOT      ("√",  " <tt>√</tt>(",                         "sqrt("), //rename later
+    EXPONENT_10      ("⑽",  " <tt><small>10ˣ</small></tt>(",        "10^("),//rename later
+    LOGARITHM_10     ("㏒",  " <tt><small>log₁₀</small></tt>(",       "log10("), //rename later
+    MODULUS          ("%",  " <tt><small>mod</small></tt> ",          "%"),
     ADDITION         ("+",  " + ",                                  "+"),
     SUBTRACTION      ("—",  " – ",                                  "-"),
     MULTIPLICATION   ("×",  " × ",                                  "*"),
