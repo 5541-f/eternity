591d46e2716e48e0476b0655652b7d17d2605117
Dick Smith <git@dicksmith.ca>
2016-02-21 21:45:22 -0500

    no message

--- app/src/main/java/comp5541/team_f/eternity/Functions.java
+++ app/src/main/java/comp5541/team_f/eternity/Functions.java
@@ -43,6 +43,56 @@ public class Functions {
   }
 
 
+  public static double pi_2(String[] args) {
+    int    count       = 999999999;
+    double pi          = 0;
+    double denominator = 1;
+    for (int x = 0; x < count; x++) {
+      if (x % 2 == 0) {
+        pi = pi + (1 / denominator);
+      } else {
+        pi = pi - (1 / denominator);
+      }
+      denominator = denominator + 2;
+    }
+    pi = pi * 4;
+    return pi;
+  }
+
+  public static double pi_3() {
+    //LeibnizFormula
+    int    count       = 999999999;
+    double pi          = 0;
+    double denominator = 1;
+    for (int x = 0; x < count; x++) {
+      if (x % 2 == 0) {
+        pi = pi + (1 / denominator);
+      } else {
+        pi = pi - (1 / denominator);
+      }
+      denominator = denominator + 2;
+    }
+    pi = pi * 4;
+    return pi;
+  }
+
+  public static double pi_4() {
+    //LeibnizFormula
+    int    count       = 999999999;
+    double pi          = 0;
+    double denominator = 1;
+    for (int x = 0; x < count; x++) {
+      if (x % 2 == 0) {
+        pi = pi + (1 / denominator);
+      } else {
+        pi = pi - (1 / denominator);
+      }
+      denominator = denominator + 2;
+    }
+    pi = pi * 4;
+    return pi;
+  }
+
   static double sin_1(double x) {
     double result = 0;
     double term;
@@ -69,8 +119,7 @@ public class Functions {
     return result;
   }
 
-  //////////////////////////////////////OTHER FUNCTIONS///////////////////////////////////////////////
-  public static double findSqr(double number) {
+  public static double squareRoot_2(double number) {
     double num    = number;
     double result = 0;
     double sqr;
@@ -91,55 +140,15 @@ public class Functions {
     return result;
   }
 
-  public static double piCal1(String[] args) {
-
-    int    count       = 999999999;
-    double pi          = 0;
-    double denominator = 1;
-
-    for (int x = 0; x < count; x++) {
-
-      if (x % 2 == 0) {
-        pi = pi + (1 / denominator);
-      } else {
-        pi = pi - (1 / denominator);
-      }
-      denominator = denominator + 2;
-    }
-    pi = pi * 4;
-    return pi;
-  }
-
-
-  public static double powCaculatorE(String[] args) {
-    System.out.println("please type one number to calculater the power of " + "10^");
-    Scanner input = new Scanner(System.in);
-    double  num   = input.nextDouble();
-    input.close();
-
-    double result = power10(num);
-    System.out.println("10^ " + num + " = " + result);
-
-    System.out.println("Calculating (10^PI and 10^-PI) ....");
-    double resultPi = power10(pinum());
-    System.out.println("By using Pi to test " + "10^PI = " + resultPi);
-    System.out.println("By using Pi to test " + "10^-PI = " + 1 / resultPi);
-    return resultPi;
-  }
-
-
   public static double power10(double exponent) {
     double result     = 1.0;
     int    largestNum = 999999999;
-
-
     if (exponent == 0) {
       result = 1.0;
     }
     if (exponent == 1) {
       result = 10.0;
     } else {
-
       for (int i = 0; i < largestNum; i++) {
         // here using Math.log() method temporarily
         // considering Dana code log() function
@@ -151,100 +160,30 @@ public class Functions {
     return result;
   }
 
-  public static double pinum() {
-    //LeibnizFormula
-    int    count       = 999999999;
-    double pi          = 0;
-    double denominator = 1;
-
-    for (int x = 0; x < count; x++) {
-
-      if (x % 2 == 0) {
-        pi = pi + (1 / denominator);
-      } else {
-        pi = pi - (1 / denominator);
-      }
-      denominator = denominator + 2;
-    }
-    pi = pi * 4;
-    return pi;
-  }
-
-}
-
-public class powCaculatorTaylor {
-
-  public static void main(String[] args) {
-
-
-    System.out.println("please type one number to calculater the power of " + "10^");
-    Scanner input = new Scanner(System.in);
-    double  num   = input.nextDouble();
-    input.close();
-
-    double result = power10(num);
-    System.out.println("10^ " + num + " = " + result);
-
-    System.out.println("Calculating (10^PI and 10^-PI) ....");
-    double resultPi = power10(pinum());
-    System.out.println("By using Pi to test " + "10^PI = " + resultPi);
-    System.out.println("By using Pi to test " + "10^-PI = " + 1 / resultPi);
-  }
-
-
   public static double power10(double exponent) {
-
     double sum          = 1;
     double elem         = 1;
     double exponentTemp = exponent;
     double numLn10;
     int    i            = 1;
-
     if (exponent == 0) {
       return 1.0;
     }
-
     if (exponent == 1) {
       return 10.0;
     }
-
     if (exponent < 0) {
       exponentTemp = -exponent;
     }
-
     numLn10 = exponentTemp * Math.log(10);
     while (elem > 1e-12) {
       elem *= numLn10 / i;
       sum += elem;
       i++;
     }
-
     if (exponent < 0) {
       sum = 1 / sum;
     }
-
     return sum;
   }
-
-  public static double pinum() {
-    //LeibnizFormula
-    int    count       = 999999999;
-    double pi          = 0;
-    double denominator = 1;
-
-    for (int x = 0; x < count; x++) {
-
-      if (x % 2 == 0) {
-        pi = pi + (1 / denominator);
-      } else {
-        pi = pi - (1 / denominator);
-      }
-      denominator = denominator + 2;
-    }
-    pi = pi * 4;
-    return pi;
-  }
-
-
-}
 }
