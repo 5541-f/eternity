5bf6acc3350573de76b9667a9acb1702e37026db
Dick Smith <git@dicksmith.ca>
2016-02-22 18:07:23 -0500

    no message

--- app/src/main/java/comp5541/teamf/eternity/Math.java
+++ app/src/main/java/comp5541/teamf/eternity/Math.java
@@ -2,14 +2,57 @@ package comp5541.teamf.eternity;
 
 public class Math {
 
-  static long factorial_1(int limit) { //Recursive function to compute the factorial of x
+  private static final double PI = 0; //fix
+  private static final double E = 0; //fix
+
+  private final double exponentiation(double base, double exponent) {
+
+    return 0; //fix
+  }
+
+  private final long factorial(int limit) {
+
+    return 0; //fix
+  }
+
+  public final double exponent10(double exponent) {
+
+    return 0; //fix
+  }
+
+  public final double exponentNatural(double exponent) {
+
+    return 0; //fix
+  }
+
+  public final double logrithm10(double exponent) {
+
+    return 0; //fix
+  }
+
+  private final double logrithmNatural(double exponent) {
+
+    return 0; //fix
+  }
+
+  public final double sine(double exponent) {
+
+    return 0; //fix
+  }
+
+  public final double squareRoot(double exponent) {
+
+    return 0; //fix
+  }
+
+  private long factorial_1(int limit) { //Recursive function to compute the factorial of x
     if (limit == 0) {
       return 1;
     }
-    return (limit * factorial_1(limit - 1));
+    return (limit * factorial(limit - 1));
   }
 
-  static long factorial_2(int limit) {
+  private long factorial_2(int limit) {
     assert limit >= 0 : limit + " is not a natural number.";
     long result = 1;
     for (int i = 1; i <= limit; i++){
@@ -18,7 +61,7 @@ public class Math {
     return result;
   }
 
-  static double natural_1() {
+  private double natural_1() {
     double result = 1;
     for (int i = 1; i < 18; i++){
       result += (1D / (factorial_2(i)));
@@ -26,7 +69,7 @@ public class Math {
     return result;
   }
 
-  static double exponentiation_1(double base, int exponent) { //Computes x^y when y is an integer
+  private double exponentiation_1(double base, int exponent) { //Computes x^y when y is an integer
     double result = 1;
     if (exponent == 0) {
       return 1;
@@ -37,7 +80,7 @@ public class Math {
     return result;
   }
 
-  public static double exponent10_1(double exponent) {
+  private static double exponent10_1(double exponent) {
     double result     = 1.0;
     int    largestNum = 999999999;
     if (exponent == 0) {
@@ -54,7 +97,7 @@ public class Math {
     return result;
   }
 
-  public static double exponent10_2(double exponent) {
+  private static double exponent10_2(double exponent) {
     double sum          = 1;
     double elem         = 1;
     double exponentTemp = exponent;
@@ -81,11 +124,11 @@ public class Math {
     return sum;
   }
 
-  static double logrithm10_1(double x) {
+  private double logrithm10_1(double x) {
     return logarithmNatural_1(x) / (logarithmNatural_1(10));
   }
 
-  static double logarithmNatural_1(double x) {
+  private double logarithmNatural_1(double x) {
     double result = 0;
     //Assumed 500 terms was a reasonable enough of an approximation
     for (int i = 0; i < 1000; i++) {
@@ -95,7 +138,7 @@ public class Math {
   }
 
   //Approximation correct to 11 decimal places using 21 terms
-  static double pi_1() {
+  private double pi_1() {
     double term = 0;
     for (int i = 0; i < 40; i++) {
       term += (exponentiation_1(-1.0 / 3.0, i)) / (2 * i + 1);
@@ -109,24 +152,24 @@ public class Math {
 //  Math.PI: 3.141592653589793
 //  Math.pi_2(): 3.1415926545880506
 //  Runtime in milliseconds: 18546
-//  public static double pi_2() {
-//    //LeibnizFormula
-//    int    count       = 999999999;
-//    double pi          = 0;
-//    double denominator = 1;
-//    for (int x = 0; x < count; x++) {
-//      if (x % 2 == 0) {
-//        pi = pi + (1 / denominator);
-//      } else {
-//        pi = pi - (1 / denominator);
-//      }
-//      denominator = denominator + 2;
-//    }
-//    pi = pi * 4;
-//    return pi;
-//  }
+  private static double pi_2() {
+    //LeibnizFormula
+    int    count       = 999999999;
+    double pi          = 0;
+    double denominator = 1;
+    for (int x = 0; x < count; x++) {
+      if (x % 2 == 0) {
+        pi = pi + (1 / denominator);
+      } else {
+        pi = pi - (1 / denominator);
+      }
+      denominator = denominator + 2;
+    }
+    pi = pi * 4;
+    return pi;
+  }
 
-  static double sin_1(double x) {
+  private double sin_1(double x) {
     double result = 0;
     double term;
     for (int i = 1; i < 65; i += 2) { //First 32 terms of the Taylor Series Expansion
@@ -140,7 +183,7 @@ public class Math {
     return result;
   }
 
-  static double squareRoot_1(double x) {
+  private double squareRoot_1(double x) {
     double sqr = x / 2;
     double temp;
     do {
@@ -150,7 +193,7 @@ public class Math {
     return temp;
   }
 
-  public static double squareRoot_2(double number) {
+  private static double squareRoot_2(double number) {
     double num    = number;
     double result = 0;
     double sqr;
