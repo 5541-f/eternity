376b5de44dd108f43d7e717196b873d074330bb1
Karl Samaha <karlsamaha@gmail.com>
2016-02-21 19:36:36 -0500

    cleaned up functions and put everything in Functions.java class

--- app/src/main/java/comp5541/team_f/eternity/Functions.java
+++ app/src/main/java/comp5541/team_f/eternity/Functions.java
@@ -1,22 +1,20 @@
 package comp5541.team_f.eternity;
 
-import net.objecthunter.exp4j.ExpressionBuilder;
-import net.objecthunter.exp4j.function.Function;
 
 public class Functions {
 
 
-  static double ln(double x){
+  static double logarithmNatural(double x){
     double num = 0;
     //Assumed 500 terms was a reasonable enough of an approximation
-    for(int i=0; i<500; i++){
+    for(int i=0; i<1000; i++){
      num += (1.0/(2*i+1))*toThePower((x-1)/(x+1),(2*i+1));
     }
     return 2*num;
   }
 
   static double log(double x){
-    return ln(x)/(ln(10));
+    return logarithmNatural(x)/(logarithmNatural(10));
   }
 
 
@@ -27,41 +25,33 @@ public class Functions {
     return (x * factorial(x - 1));
   }
 
-  static double toThePower(double x, int y) { //Computes x^y when y is an integer
+  static double toThePower(double base, int exponent) { //Computes x^y when y is an integer
     double result = 1;
-    if (y == 0) {
+    if (exponent == 0) {
       return 1;
     }
-    for (int i = 0; i < y; i++) {
-      result *= x;
+    for (int i = 0; i < exponent; i++) {
+      result *= base;
     }
     return result;
   }
 
   //Approximation correct to 11 decimal places using 21 terms
   static double pi() {
-
     double term = 0;
-    double sqrt12 = new ExpressionBuilder("sqrt(12)")
-            .function(sqrt)
-            .build()
-            .evaluate();
-
     for (int i = 0; i < 40; i++) {
       term += (toThePower(-1.0 / 3.0, i)) / (2 * i + 1);
     }
-    return sqrt12 * term;
+    return squareRoot(12) * term;
   }
 
 
-  static Function sin = new Function("sin") {
-    @Override
-    public double apply(double... args) {
+  static double sin(double x){
       double result = 0;
       double term;
 
       for (int i = 1; i < 65; i += 2) { //First 32 terms of the Taylor Series Expansion
-        term = toThePower(pi() / 180, i) * toThePower(args[0], i) / factorial(i); //
+        term = toThePower(pi() / 180, i) * toThePower(x, i) / factorial(i); //
         if (i % 4 == 1) {
           result += term;
         } else {
@@ -70,30 +60,217 @@ public class Functions {
       }
       return result;
     }
-  };
 
-  static Function sqrt = new Function("sqrt") {
-    @Override
-    public double apply(double... args) {
+
+  static double squareRoot(double x){
       double result;
       double sqr;
       double temp;
-      if (args[0] == 0) {
-        sqr = 0;
-        result = sqr;
-      } else if (args[0] < 0) {
-        result = -99999999;
-      } else {
-        sqr = args[0] / 2;
+
+        sqr = x / 2;
         do {
           temp = sqr;
-          sqr = (temp + args[0] / temp) / 2;
+          sqr = (temp + x / temp) / 2;
         } while ((temp - sqr) != 0);
         result = temp;
-      }
       return result;
     }
-  };
+//////////////////////////////////////OTHER FUNCTIONS///////////////////////////////////////////////
+
+//  public class function {
+//
+//    public static double findSqr(){
+//      Scanner sc = new Scanner(System.in);
+//      System.out.print("please enter number: " );
+//      double num = sc.nextDouble();
+//      sc.close();
+//      double result = 0;
+//      double sqr;
+//      double temp = 0;
+//      if(num == 0){
+//        sqr = 0;
+//        result = sqr;
+//      }
+//      else if (num < 0){
+//        result = -99999999;
+//      }
+//      else{
+//        sqr = num/2;
+//        do{
+//          temp = sqr;
+//          sqr = (temp+num/temp)/2;
+//        }while((temp-sqr)!=0);
+//        result = temp;
+//      }
+//      return result;
+//    }
+//    public static void main(String[] args){
+//      double result = findSqr();
+//      System.out.println("   the square root is +" + result);
+//      System.out.println("or the square root is -" + result);
+//    }
+
+// Calculating pi with Leibniz
+// public class piCal1 {
+//    public static void main(String[] args) {
+//
+//      int count = 999999999;
+//      double pi = 0;
+//      double denominator = 1;
+//
+//      for (int x = 0; x < count; x++) {
+//
+//        if (x % 2 == 0) {
+//          pi = pi + (1 / denominator);
+//        } else {
+//          pi = pi - (1 / denominator);
+//        }
+//        denominator = denominator + 2;
+//      }
+//      pi = pi * 4;
+//      System.out.println(pi);
+//    }
+//
+//  }
+
+//  public class powCaculatorE {
+//
+//    public static void main (String[] args){
+//
+//
+//      System.out.println("please type one number to calculater the power of " + "10^");
+//      Scanner input = new Scanner (System.in);
+//      double num = input.nextDouble();
+//      input.close();
+//
+//      double result = power10(num);
+//      System.out.println("10^ " + num + " = "+ result);
+//
+//      System.out.println("Calculating (10^PI and 10^-PI) ...." );
+//      double resultPi = power10(pinum());
+//      System.out.println("By using Pi to test " + "10^PI = " + resultPi);
+//      System.out.println("By using Pi to test " + "10^-PI = " + 1/resultPi);
+//    }
+//
+//
+//    public static double power10(double exponent){
+//      double result = 1.0;
+//      int largestNum = 999999999;
+//
+//
+//      if (exponent == 0){
+//        result = 1.0;
+//      }
+//      if (exponent ==1){
+//        result = 10.0;
+//      }else{
+//
+//        for(int i = 0; i<largestNum; i++){
+//          // here using Math.log() method temporarily
+//          // considering Dana code log() function
+//          // finally will call Dana's log()
+//          double part = exponent * Math.log(10);
+//          result *= (1+part/largestNum);
+//        }
+//      }
+//      return result;
+//    }
+//
+//    public static double pinum(){
+//      //LeibnizFormula
+//      int count = 999999999;
+//      double pi = 0;
+//      double denominator = 1;
+//
+//      for (int x = 0; x < count; x++) {
+//
+//        if (x % 2 == 0) {
+//          pi = pi + (1 / denominator);
+//        } else {
+//          pi = pi - (1 / denominator);
+//        }
+//        denominator = denominator + 2;
+//      }
+//      pi = pi * 4;
+//      return pi;
+//    }
+//
+//  }
+//public class powCaculatorTaylor {
+//
+//  public static void main (String[] args){
+//
+//
+//    System.out.println("please type one number to calculater the power of " + "10^");
+//    Scanner input = new Scanner (System.in);
+//    double num = input.nextDouble();
+//    input.close();
+//
+//    double result = power10(num);
+//    System.out.println("10^ " + num + " = "+ result);
+//
+//    System.out.println("Calculating (10^PI and 10^-PI) ...." );
+//    double resultPi = power10(pinum());
+//    System.out.println("By using Pi to test " + "10^PI = " + resultPi);
+//    System.out.println("By using Pi to test " + "10^-PI = " + 1/resultPi);
+//  }
+//
+//
+//  public static double power10(double exponent){
+//
+//    double sum =1;
+//    double elem =1;
+//    double exponentTemp = exponent;
+//    double numLn10;
+//    int i=1;
+//
+//    if (exponent == 0){
+//      return 1.0;
+//    }
+//
+//    if (exponent ==1){
+//      return 10.0;
+//    }
+//
+//    if (exponent < 0){
+//      exponentTemp = -exponent;
+//    }
+//
+//    numLn10= exponentTemp * Math.log(10);
+//    while(elem>1e-12){
+//      elem *= numLn10/i;
+//      sum += elem;
+//      i++;
+//    }
+//
+//    if  (exponent < 0){
+//      sum = 1/sum;
+//    }
+//
+//    return sum;
+//  }
+//
+//  public static double pinum(){
+//    //LeibnizFormula
+//    int count = 999999999;
+//    double pi = 0;
+//    double denominator = 1;
+//
+//    for (int x = 0; x < count; x++) {
+//
+//      if (x % 2 == 0) {
+//        pi = pi + (1 / denominator);
+//      } else {
+//        pi = pi - (1 / denominator);
+//      }
+//      denominator = denominator + 2;
+//    }
+//    pi = pi * 4;
+//    return pi;
+//  }
+//
+//
+//}
 
 
   public static void main(String[] args) throws IllegalArgumentException {
@@ -103,8 +280,11 @@ public class Functions {
     //                .function(sin)
     //                .build()
     //                .evaluate();
-    System.out.println(ln(30));
+    System.out.println(logarithmNatural(30));
     System.out.println(log(100));
+    System.out.println(squareRoot(49));
+    System.out.println(sin(45));
+    System.out.println();
     //        double sqrt9 = new ExpressionBuilder("sqrt(12)")
     //                .function(sqrt)
     //                .build()
